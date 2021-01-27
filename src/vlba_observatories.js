b6138ccced80b1f6ccd36b4be6d486a6790a4606
Jane Kaczmarek <jane.kaczmarek@gmail.com>
2021-01-27 14:25:14 -0800

    Update and rename observatories.js to vlba_observatories.js

---
+++ src/vlba_observatories.js
@@ -0,0 +1,64 @@
+// Some locations we know about.
+// All values in degrees.
+export default {
+  SC: {
+    name: "StCroix",
+    latitude: 17.756578,
+    longitude: -64.583631,
+    elevationLimit: 12.0
+  },
+  HN: {
+    name: "Hancock",
+    latitude: 42.933608,
+    longitude: -71.986581,
+    elevationLimit: 12.0
+  },
+  NL: {
+    name: "NorthLiberty",
+    latitude: 41.771425,
+    longitude: -91.574133,
+    elevationLimit: 12.0
+  },
+  FD: {
+    name: "FortDavis",
+    latitude: 30.635031,
+    longitude: -103.944817,
+    elevationLimit: 12.0
+  },
+  LA: {
+    name: "LosAlamos",
+    latitude: 35.775125,
+    longitude: -106.245597,
+    elevationLimit: 12.0
+  },
+  PT: {
+    name: "PieTown",
+    latitude: 34.301003,
+    longitude: -108.119183,
+    elevationLimit: 12.0
+  },
+  KP: {
+    name: "KittPeak",
+    latitude: 31.956306,
+    longitude: -111.612422,
+    elevationLimit: 12.0
+  },
+  OV: {
+    name: "OwensValley",
+    latitude: 37.231653,
+    longitude: -118.277047,
+    elevationLimit: 12.0
+  },
+    BR: {
+    name: "Brewster",
+    latitude: 48.131228,
+    longitude: -119.683278,
+    elevationLimit: 12.0
+  },
+    MK: {
+    name: "MaunaKea",
+    latitude: 19.801381,
+    longitude: -155.455503,
+    elevationLimit: 12.0
+  }
+};
